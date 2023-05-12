bbaffc6285a0968fbcae1b94501e4d340e865070
Jetlonglim <jetlong1628@gmail.com>
2023-05-12 15:51:24 +0800

    My first tkinter and python

--- myfirstGit.py
+++ myfirstGit.py
@@ -1,1 +1,17 @@
-print("hello world!!!")
+import tkinter as tk
+
+# Create a new Tkinter window
+window = tk.Tk()
+
+# Set the window title
+window.title("Simple Tkinter Window")
+print('hello world!!')
+# Set the window dimensions
+window.geometry("400x300")
+
+# Add a label widget
+label = tk.Label(window, text="Hello, Tkinter!")
+label.pack()
+
+# Start the Tkinter event loop
+window.mainloop()
